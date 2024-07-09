40172f39ef4eee7dcb5b2d3fe9cc9ae8d67e4068
Secondyou <txwfyxa@gmail.com>
2024-07-09 17:30:06 +0700

    Release Revison 1

--- src/main/java/net/secondyou/safedisconnect/SecondyouSafedisconnect.java
+++ src/main/java/net/secondyou/safedisconnect/SecondyouSafedisconnect.java
@@ -9,7 +9,7 @@ public class SecondyouSafedisconnect implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("secondyou-safedisconnect");
+    public static final Logger LOGGER = LoggerFactory.getLogger("Safe Disconnect");
 
 	@Override
 	public void onInitialize() {
