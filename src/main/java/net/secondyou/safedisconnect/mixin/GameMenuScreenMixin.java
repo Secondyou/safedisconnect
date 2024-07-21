a3580393243ba318228c222ebd05ec60b2984799
Secondyou <txwfyxa@gmail.com>
2024-07-21 09:03:29 +0700

    1.20.X Test

--- src/main/java/net/secondyou/safedisconnect/mixin/GameMenuScreenMixin.java
+++ src/main/java/net/secondyou/safedisconnect/mixin/GameMenuScreenMixin.java
@@ -1,40 +1,43 @@
 package net.secondyou.safedisconnect.mixin;
 
 import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.ButtonWidget.PressAction;
 import net.minecraft.text.Text;
-import net.minecraft.client.MinecraftClient;
 import net.secondyou.safedisconnect.ConfirmDisconnectScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameMenuScreen.class)
-public class GameMenuScreenMixin {
+public class GameMenuScreenMixin extends Screen {
 
-    @ModifyArg(
-            method = "initWidgets()V",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/widget/ButtonWidget;builder(Lnet/minecraft/text/Text;Lnet/minecraft/client/gui/widget/ButtonWidget$PressAction;)Lnet/minecraft/client/gui/widget/ButtonWidget$Builder;"
-            ),
-            index = 1
-    )
-    private PressAction modifyDisconnectButton(PressAction original) {
-        return button -> {
-            Text disconnectText = Text.translatable("menu.disconnect");
-            if (button.getMessage().getString().equals(disconnectText.getString())) {
-                GameMenuScreen thisScreen = (GameMenuScreen) (Object) this;
-                MinecraftClient client = MinecraftClient.getInstance();
-                client.setScreen(new ConfirmDisconnectScreen(thisScreen, () -> {
-                    if (client.world != null) {
-                        client.world.disconnect();
-                    }
-                }));
-            } else {
-                original.onPress(button);
-            }
-        };
+    protected GameMenuScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "initWidgets", at = @At("RETURN"))
+    private void initWidgets(CallbackInfo ci) {
+        // Find and replace the disconnect button
+        this.children().stream()
+                .filter(element -> element instanceof ButtonWidget)
+                .map(element -> (ButtonWidget) element)
+                .filter(button -> button.getMessage().equals(Text.translatable("menu.disconnect")))
+                .findFirst()
+                .ifPresent(button -> {
+                    int index = this.children().indexOf(button);
+                    ButtonWidget newButton = ButtonWidget.builder(Text.translatable("menu.disconnect"), b ->
+                                    this.client.setScreen(new ConfirmDisconnectScreen(this, confirmButton -> {
+                                        if (this.client.world != null) {
+                                            this.client.world.disconnect();
+                                        }
+                                        this.client.disconnect();
+                                    })))
+                            .dimensions(button.getX(), button.getY(), button.getWidth(), button.getHeight())
+                            .build();
+                    this.remove(button);
+                    this.addDrawableChild(newButton);
+                });
     }
 }
